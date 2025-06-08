4437b054a643d22e8a105600a630f4f31136b87c
gmotta <gmotta.dev@outlook.com>
2025-06-08 19:43:51 -0300

    Update nav-anchors to use absolute paths for internal links, improving navigation consistency.

--- src/app/_layout-resources/Nav/nav-anchors.ts
+++ src/app/_layout-resources/Nav/nav-anchors.ts
@@ -1,6 +1,6 @@
 export const navAnchors = [
   { label: "Blog", href: "/blog" },
-  { label: "Sobre mim", href: "#sobre-mim" },
-  { label: "Atendimento", href: "#atendimento" },
-  { label: "Contato", href: "#contato" },
+  { label: "Sobre mim", href: "/#sobre-mim" },
+  { label: "Atendimento", href: "/#atendimento" },
+  { label: "Contato", href: "/#contato" },
 ];
